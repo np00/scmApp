a7e533045573bd21e8f93d28175427658e211307
np00 <niklas.petersen@openmailbox.org>
2016-09-23 13:57:01 +0200

    example rest scor query

--- js/scm-app.js
+++ js/scm-app.js
@@ -20,23 +20,22 @@ define(['jquery', 'lib/rdfstore', 'logger', 'lib/vis.min'],
   edges = new vis.DataSet();
 
 
+
+
   var getProcesses = function() 
   {
-      var query = "query=%20CONSTRUCT%20%7B%3Fs%20%3Fp%20%3Fo%7D%20WHERE%20%20%20%20%7B%20%3Fs%20a%20%3Chttp%3A%2F%2Fpurl.org%2Feis%2Fvocab%2Fscor%23Process%3E%20.%20%20%20%20%3Fs%20%3Fp%20%3Fo%7D";
+      var getProcesses_query = "query=%20CONSTRUCT%20%7B%3Fs%20%3Fp%20%3Fo%7D%20WHERE%20%20%20%20%7B%20%3Fs%20a%20%3Chttp%3A%2F%2Fpurl.org%2Feis%2Fvocab%2Fscor%23Process%3E%20.%20%20%20%20%3Fs%20%3Fp%20%3Fo%7D";
 
-      var xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
-
-      xhr.open("POST", "https://lucid.eccenca.com/dataplatform/proxy/default/sparql?access_token=4248770c-e540-48b7-9191-365e8bd4e62b");
+      var xhr;
+      xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://lucid.eccenca.com/dataplatform/proxy/default/sparql?access_token=cc91bbee-6739-4ba1-9bcc-006c2bd2b86e");
       xhr.setRequestHeader("accept", "application/ld+json");
       xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-      xhr.setRequestHeader("referer", "https://lucid.eccenca.com/");
-      xhr.setRequestHeader("connection", "keep-alive");
-      xhr.send(query);
+      xhr.send(getProcesses_query);
 
       xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-          console.log(this.responseText);
+
 
           var data = JSON.parse(this.responseText);
 
@@ -56,88 +55,141 @@ define(['jquery', 'lib/rdfstore', 'logger', 'lib/vis.min'],
             addEdge(i, processes[i].hasClient, processes[i].hasSupplier, processes[i]["hasCommitDate"]);
 
           });
-
-
         }
       });
 
-//  var edges = new vis.DataSet([
-//    {from: 1, to: 8, arrows:'to', },
- 
+
+      runMetric();
+  }
 
 
+   var runMetric = function() 
+  {
+        var delivery_in_full_query = "query=PREFIX%20%3A%20%3Chttp%3A%2F%2Fpurl.org%2Feis%2Fvocab%2Fscor%23%3E%0APREFIX%20xsd%3A%20%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0ASELECT%20(((%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23double%3E(%3Fvalue1))%20%2B%20(%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23double%3E(%3Fvalue2)))%20%2F%202%20AS%20%3FmetricResult)%20FROM%20%3CsupplyChain%3E%0AWHERE%20%7B%0A%20%20%3Forder%20%3Chttp%3A%2F%2Fpurl.org%2Feis%2Fvocab%2Fscor%23hasMetricRL_33%3E%20%3Fvalue1.%0A%20%20%3Forder%20%3Chttp%3A%2F%2Fpurl.org%2Feis%2Fvocab%2Fscor%23hasMetricRL_50%3E%20%3Fvalue2.%0A%7D%0ALIMIT%2010";
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.open("POST", "https://lucid.eccenca.com/dataplatform/proxy/default/sparql?access_token=cc91bbee-6739-4ba1-9bcc-006c2bd2b86e");
+        xhr.setRequestHeader("accept", "application/sparql-results+json");
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");   
+        xhr.send(delivery_in_full_query);
+
+        xhr.addEventListener("readystatechange", function () {
+          if (this.readyState === 4) {
+            console.log(this.responseText);
+          }
+        });
+    }
+
+
+  function readTextFile(file)
+  {
+      var rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, false);
+      rawFile.onreadystatechange = function ()
+      {
+          if(rawFile.readyState === 4)
+          {
+              if(rawFile.status === 200 || rawFile.status == 0)
+              {
+                  var allText = rawFile.responseText;
+                  
+                  var query = htmlEscape(allText);
+
+                  return query;
+              }
+          }
+      }
+      rawFile.send(null);
+  }
+
+
+  function htmlEscape(str) {
+    return str
+        .replace(/&/g, '&amp;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/{/g, '&#123;')
+        .replace(/}/g, '&#125;')
+        .replace(/\(/g, '&#40;')
+        .replace(/\)/g, '&#41;')
+        .replace(/:/g, '&#58;')
+        .replace(/\;/g, '&#59;')
+        .replace(/\./g, '&#46;')
+        .replace(/\?/g, '&#63;')
+        ;
   }
 
     var createGraph = function () {
 
-  var data = {
-    nodes: nodes,
-    edges: edges
+    var data = {
+      nodes: nodes,
+      edges: edges
+    };
+
+    var input = 'RL';
+    var options = {  layout: {
+                        hierarchical: {
+                          direction: 'LR'}},
+                     nodes:{  
+                          color: {
+                              background: 'pink',
+                              border: 'purple'}},
+                    edges:{
+                    font: {
+        color: 'green',
+        size: 18, // px
+        face: 'arial',
+        background: 'white',
+        strokeWidth: 2, // px
+        strokeColor: '#ffffff',
+        align:'horizontal'
+      }}
+    
+    };
+    var network = new vis.Network(container, data, options);
+
+     network.on("click", function (params) {
+          params.event = "[original event]";
+      })
+
   };
 
-  var input = 'RL';
-  var options = {  layout: {
-                      hierarchical: {
-                        direction: 'LR'}},
-                   nodes:{  
-                        color: {
-                            background: 'pink',
-                            border: 'purple'}},
-                  edges:{
-                  font: {
-      color: 'green',
-      size: 18, // px
-      face: 'arial',
-      background: 'white',
-      strokeWidth: 2, // px
-      strokeColor: '#ffffff',
-      align:'horizontal'
-    }}
-  
-  };
-  var network = new vis.Network(container, data, options);
 
-   network.on("click", function (params) {
-        params.event = "[original event]";
-    })
+  function addNode(id, label) {
+      try {
+          nodes.add({
+              id: id,
+              label: label
+          });
+      }
+      catch (err) {
+          alert(err);
+      }
 
-};
+     createGraph();
+  }
 
 
-        function addNode(id, label) {
-            try {
-                nodes.add({
-                    id: id,
-                    label: label
-                });
-            }
-            catch (err) {
-                alert(err);
-            }
-
-           createGraph();
-        }
-
-
-        function addEdge(id, from, to, label) {
-            try {
-                edges.add({
-                    id: id,
-                    from: from, 
-                    to: to,
-                    label: label
-                });
-            }
-            catch (err) {
-                alert(err);
-            }
-        }
+  function addEdge(id, from, to, label) {
+      try {
+          edges.add({
+              id: id,
+              from: from, 
+              to: to,
+              label: label
+          });
+      }
+      catch (err) {
+          alert(err);
+      }
+  }
      
-
-
 // button clicks
 buttonAnalytics.bind("click", getProcesses);
-
 
 
 // onload
